6808f1e8e8b15a964826ceb275b94c75ef557e9a
30-Harshitha <harshitha.haranahalli@gmail.com>
2026-07-17 10:09:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (16.23%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]>='A'&&s[i]<='Z')
+            {
+                s[i]=('a'-'A')+s[i];
+            }
+        }
+        return s;
+   
+}
+
+};
