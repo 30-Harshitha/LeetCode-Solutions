2968fccf13e08b765120de514bd295678ca91f5f
30-Harshitha <harshitha.haranahalli@gmail.com>
2026-07-20 13:13:19 +0530

    Time: 8 ms (12.04%) | Memory: 46.9 MB (7.56%) - LeetSync

---
+++ 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String removeTrailingZeros(String num) {
+        while(num.length()>0&&num.charAt(num.length()-1)=='0')
+        {
+            num=num.substring(0,num.length()-1);
+        }
+        return num;
+    }
+}
