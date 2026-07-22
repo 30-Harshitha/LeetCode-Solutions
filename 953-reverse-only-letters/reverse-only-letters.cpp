15589c662c6396c292bd102b8e9e10e607f7b659
30-Harshitha <harshitha.haranahalli@gmail.com>
2026-07-22 11:08:38 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (82.35%) - LeetSync

---
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int i = 0;
+        int j = s.size() - 1;
+
+        while (i < j) {
+            if (isalpha(s[i]) && isalpha(s[j])) {
+                swap(s[i], s[j]);
+                i++;
+                j--;
+            }
+            else if (isalpha(s[i]) && !isalpha(s[j])) {
+                j--;
+            }
+            else if (!isalpha(s[i]) && isalpha(s[j])) {
+                i++;
+            }
+            else {
+                i++;
+                j--;
+            }
+        }
+
+        return s;
+    }
+};
