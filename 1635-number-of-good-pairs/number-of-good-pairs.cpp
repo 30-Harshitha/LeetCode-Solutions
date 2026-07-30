46b780d4db1a940bae587358bfe1e5e9dbe01810
30-Harshitha <harshitha.haranahalli@gmail.com>
2026-07-30 11:01:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (24.28%) - LeetSync

---
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int,int> freq;
+        int ans=0;
+        for(auto a:nums)
+        {
+            ans += freq[a];
+            freq[a]++;
+        }
+        return ans;
+    }
+};
